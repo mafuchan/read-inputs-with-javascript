3bee6dff54ca0b1db4e5c7b55cb0f18eedb9a689
Matt Crecraft <mcrecraft@gmail.com>
2022-05-14 22:02:59 -0500

    Solution

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function logValues(){
-  // Your code here
+function logValues() {
+  const form = document.querySelector("form")
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const firstName = formData.get("first-name")
+    const lastName = formData.get("last-name")
+    const email = formData.get("email")
+
+    localStorage.setItem("first-name", firstName)
+    localStorage.setItem("last-name", lastName)
+    localStorage.setItem("email", email)
+
+    event.target.reset()
+    console.log(localStorage)
+  })
 }
